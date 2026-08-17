8f2730179a2b5ce32905887a955bf32b144297e8
Mr-mnin <nadimul.nahin008@gmail.com>
2026-08-17 13:40:31 +0600

    17aug

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from rest_framework import viewsets, permissions
+from .models import *
+from .serializer import *
+from rest_framework.response import Response
 
-# Create your views here.
+# Create your views here
+
+class CountryViewSet(viewsets.ModelViewSet):
+    permission_classes = [permissions.AllowAny]
+    queryset = Country.objects.all()
+    serializer_class = CountrySerializer
+
